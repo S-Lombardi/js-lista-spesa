b5124628051c5f8e6727cc69501c406194823c94
S-Lombardi <silo.lombardi@gmail.com>
2023-05-24 18:51:39 +0200

    Inserimento campo input e pulsante per aggiungere elementi

---
+++ bonus/js/script.js
@@ -0,0 +1,53 @@
+"use strict";
+
+//Scrivo la lista della spesa con un array
+let lista = [
+    'Pane',
+    'Acqua',
+    'Pasta',
+    'Vino',
+    'Verdure',
+    'Cereali',
+    'Caffè',
+];
+
+//Variabile per la condizione del ciclo while
+let indice = 0;
+
+//Recupero il tag ul tramite l'id
+let ul = document.getElementById("wrap-list");
+
+//ciclo while per incrementare il numero degli elementi della lista
+while( indice < lista.length ) {
+    
+    //variabile che corrisponde al numero degli elementi presenti della lista
+    let prodottiLista = lista[indice];
+    
+    //creo i tag li e inserisco gli elementi della lista
+    let tagLista = `<li>${prodottiLista}</li>`;
+    
+    //inserisco la lista nell'ul del DOM
+    ul.innerHTML += tagLista;
+    
+    //il contatore incrementa di uno fino a quando la condizione risulta vera
+    indice++
+}
+
+//recupero il pulsante e l'input
+const buttonAdd = document.getElementById("add-item");
+const inputAdd = document.getElementById("input-item");
+
+
+buttonAdd.addEventListener("click", function() {
+    
+    //recupero il valore nel campo input
+    let newItem = inputAdd.value ;
+
+    //inserisco i nuovi elementi nell' array
+    lista.push(newItem);
+    let tagLista = `<li>${newItem}</li>`;
+    ul.innerHTML += tagLista;
+
+    inputAdd.value = '';
+
+});
---
+++ bonus/readme.md
@@ -0,0 +1,12 @@
+PROBLEMA :Data una lista della spesa, stampare sulla pagina gli elementi della lista individualmente con un ciclo while
+
+1-Creo la lista della spesa dentro un array
+
+2-Dichiaro una variabile e gli assegno 0 ( valore sul quale si basa la condizione del ciclo while)
+3-Creare un ciclo while --> variabile creata nel punto precedente deve essere < della lunghezza dell'array ( n-1 = indice array).
+4-Il  blocco di codice nel ciclo conterrà alla fine l'incremento dell' indice
+
+5-Aggiungo tag <ul> nel body dell'html e lo recupero con l'id
+6-Creo l'elemento <li>
+7-Inserisco nei tag gli elementi della lista
+9-inserisco la lista nell'html
